6b7932a2d969d31ed57964bcf4ea94e18ff94b57
matheus <mrosmaninhu@outlook.com>
2016-02-11 17:45:48 -0200

    Concatenando string e dados no print

--- teste.py
+++ teste.py
@@ -1,4 +1,7 @@
-__author__ = 'Matheus';
-a = 10;
-b = "Abc";
-print(type(b));
+num_int = 5;
+num_desc = 7.3;
+val_str = "qualquer texto";
+
+print("O valor é:", num_int);
+print("O valor é: %i" %num_int);
+print("O valor é: " + str(num_int));
