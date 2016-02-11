c445c73d2d9fe19db343b19e009cc19e6ab9d4f5
matheus <mrosmaninhu@outlook.com>
2016-02-11 17:50:55 -0200

    Concatenando decimal

--- teste.py
+++ teste.py
@@ -5,3 +5,6 @@ val_str = "qualquer texto";
 print("O valor é:", num_int);
 print("O valor é: %i" %num_int);
 print("O valor é: " + str(num_int));
+
+print("Concatenando decimal:", num_desc);
+print("Concatenando decimal: %.10f" %num_desc);
